3d9b5d52259c2c871b2e3dee16fb146f12e0ed5a
avaerian <rblxavaerian@gmail.com>
2023-03-02 01:47:50 -0500

    Test for random values for grid algorithm

--- src/test/java/GridAlgorithmTest.java
+++ src/test/java/GridAlgorithmTest.java
@@ -1,14 +1,16 @@
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.minerift.ether.GridAlgorithm;
 import org.minerift.ether.island.Tile;
 
+import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GridAlgorithmTest {
@@ -37,6 +39,25 @@ public class GridAlgorithmTest {
         final int end = 100;
         IntStream stream = IntStream.rangeClosed(start, end);
         return stream.boxed();
+    }
+
+    // Validate by ensuring the algorithm works from and to tile coordinates and id
+    // Runs through random values instead of a defined range
+    @ParameterizedTest
+    @MethodSource
+    public void random_inverseTest(int inputId) {
+        final Tile tile = grid.computeTile(inputId);
+        final int tileId = grid.computeTileId(tile);
+        System.out.println(inputId + " -> " + tile + " -> " + tileId);
+        assertEquals(tileId, inputId);
+    }
+
+    // Provides random parameters for test above
+    private static Stream<Integer> random_inverseTest() {
+        final int randomValueCount = 50;
+        final int randomBound = 50000;
+        Random random = new Random();
+        return random.ints(randomValueCount, 0, randomBound).boxed();
     }
 
 }
