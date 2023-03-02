126f3a31a6d8a41be64b54174726d7ee5cf788a3
avaerian <rblxavaerian@gmail.com>
2023-03-02 15:22:14 -0500

    Reduce code smells

--- src/main/java/org/minerift/ether/GridAlgorithm.java
+++ src/main/java/org/minerift/ether/GridAlgorithm.java
@@ -1,29 +1,25 @@
 package org.minerift.ether;
 
+import org.apache.commons.lang3.Validate;
 import org.minerift.ether.island.Tile;
 
 /**
- * Algorithm for locating a Tile
+ * Algorithm for calculating tile coordinates and tile ids.
  * @author Avaerian
  */
 public class GridAlgorithm {
 
     /**
-     * This method should not be necessary in most cases,
-     * but exists in case a tile id is forgotten and needs
-     * to be calculated from a tile.
-     *
-     * Island deletion should not require this method.
-     *
-     * Method loses accuracy after id 2147441940, and needs to be fixed.
-     *
+     * Algorithm for calculating a tile id from tile coordinates.
+     * @param tile tile coordinates
+     * @return integer id of the tile
      */
     public int computeTileId(Tile tile) {
 
-        // If positive diagonal
+        Validate.notNull(tile, "Tile cannot be null!");
+
+        // Check if tile is a positive diagonal tile
         if(tile.getX() >= 0 && tile.getX() == tile.getZ()) {
-            // This is a diagonal tile
-            // tile.x is a shellId; get diagonalId and return
             return getDiagonalId(tile.getX());
         }
 
@@ -36,22 +32,18 @@ public class GridAlgorithm {
         Tile bottomLeft  = new Tile(-shellId,  shellId);
         Tile bottomRight = new Tile( shellId,  shellId);
 
-        int numColumns = topRight.getZ() - bottomRight.getZ();
+        // int numColumns = topRight.getZ() - bottomRight.getZ();
 
+        // Check if tile is in top row
         if(isInRow(tile, topLeft, topRight)) {
-            // Is in top row; traverse
-
             // Start in top left
             int startTileId = previousDiagonalId + 1;
             startTileId += tile.getX() - topLeft.getX();
             return startTileId;
-
         }
 
-
+        // Check if tile is in bottom row
         if(isInRow(tile, bottomLeft, bottomRight)) {
-            // Is in bottom row; traverse
-
             // Start in bottom right
             int endTileId = diagonalId;
             endTileId -= bottomRight.getX() - tile.getX();
@@ -59,33 +51,30 @@ public class GridAlgorithm {
         }
 
         // Tile is in a column
+        int startTileId;
         if(tile.getX() > 0) {
             // Right
-
-            int startTileId = previousDiagonalId + 1;
+            startTileId = previousDiagonalId + 1;
             startTileId += tile.getX() - topLeft.getX();
-
             startTileId += (tile.getZ() - topRight.getZ()) * 2;
-            return startTileId;
-
         } else {
             // Left
-
-            int startTileId = previousDiagonalId;
+            startTileId = previousDiagonalId;
             startTileId += topRight.getX() - topLeft.getX();
             startTileId += (tile.getZ() - topLeft.getZ()) * 2;
-            return startTileId;
-
         }
+
+        return startTileId;
     }
 
-
     /**
-     * Algorithm for calculating tile on grid from a tile id
+     * Algorithm for calculating tile coordinates from a tile id.
      * @param tileId id of tile on grid
      * @return Tile coordinates
      */
     public Tile computeTile(int tileId) {
+
+        Validate.isTrue(tileId >= 0, "tileId needs to be a positive number!");
 
         if(tileId == 0) {
             return Tile.ZERO;
@@ -95,21 +84,14 @@ public class GridAlgorithm {
         int nextDiagonalId = getDiagonalId(shellId);
         int previousDiagonalId = getDiagonalId(shellId - 1);
 
-        // TODO: review this -> diagonal check + tile math here doesn't seem correct
-        if(tileId > 0 && previousDiagonalId == tileId) { // If id is a diagonal
-            // This is a positive diagonal tile; don't need to calculate
-            return new Tile(tileId / 8, tileId / 8);
-        }
-
         int tilesInShell = nextDiagonalId - previousDiagonalId;
         int tilesInRow = 3 + (2 * (shellId - 1));
         int tilesInColumn = tilesInShell - (tilesInRow * 2);
         int numColumns = tilesInColumn / 2;
 
-        // Start at end; will work back from there
+        // Start at end of shell; will work back from there
         Tile.Mutable tile = new Tile.Mutable(shellId, shellId);
 
-        // Determine whether tile is in row or column
         int startTopRow = previousDiagonalId + 1;
         int endTopRow = startTopRow + (tilesInRow - 1);
         int startBottomRow = nextDiagonalId - (tilesInRow - 1);
@@ -127,28 +109,18 @@ public class GridAlgorithm {
             return tile;
         }
 
-
         // Tile is in a column
-        if(tileId % 2 != 0) { // odd (right)
-
-            // endTopRow is a reference point
+        if(tileId % 2 != 0) {
+            // Odd -> right column
             int endColumn = endTopRow + (numColumns * 2);
             tile.subtract(0, 1 + ((endColumn - tileId) / 2));
-            return tile;
 
-        } else { // even (left)
-
+        } else {
+            // Even -> left column
             int endColumn = startBottomRow - 2;
             tile.subtract((tilesInRow - 1), 1 + ((endColumn - tileId) / 2));
-
         }
 
-        // If in row, check if in top or bottom row
-        // Get end of row and work backwards to get tile
-
-        // If in column, check if number is even or odd
-        // If odd, tile is on right side
-        // If even, tile is on left side
         return tile;
     }
 
@@ -164,14 +136,12 @@ public class GridAlgorithm {
     }
 
     private boolean isInRow(Tile tile, Tile left, Tile right) {
-
         // Left is negative (lower bound)
         // Right is positive (upper bound)
-
         return inRange(tile.getX(), left.getX(), right.getX()) && tile.getZ() == left.getZ();
     }
 
-
+    // Inclusive for min and max bounds
     private boolean inRange(int i, int min, int max) {
         return i >= min && i <= max;
     }
