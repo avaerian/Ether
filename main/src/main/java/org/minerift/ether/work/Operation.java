3d806d16b0e57033c030b35f8e9c84ed226214db
avaerian <55154263+avaerian@users.noreply.github.com>
2023-07-11 22:16:13 -0400

    Implement basic work queue (#23)

---
+++ main/src/main/java/org/minerift/ether/work/Operation.java
@@ -0,0 +1,76 @@
+package org.minerift.ether.work;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.function.BiConsumer;
+import java.util.function.BooleanSupplier;
+
+// Batch of work that is completed together
+public class Operation {
+
+    private Deque<BooleanSupplier> tasks;
+    private BiConsumer<Boolean, FailReason> callback;
+
+    public Operation() {
+        this(new ArrayDeque<>());
+    }
+
+    public Operation(Deque<BooleanSupplier> tasks) {
+        this.tasks = tasks;
+        this.callback = null;
+    }
+
+    public void addTask(BooleanSupplier task) {
+        tasks.add(task);
+    }
+
+    public void whenComplete(BiConsumer<Boolean, FailReason> callback) {
+        this.callback = callback;
+    }
+
+    // Fail the operation
+    public void fail(FailReason reason) {
+        Preconditions.checkNotNull(reason, "Fail reason cannot be null!");
+        callback.accept(false, reason);
+    }
+
+    // Append all tasks from other operation to this operation
+    public void join(Operation other) {
+        tasks.addAll(other.tasks);
+    }
+
+    public ImmutableList<BooleanSupplier> getRemainingTasks() {
+        return ImmutableList.copyOf(tasks);
+    }
+
+    public int getRemainingTaskCount() {
+        return tasks.size();
+    }
+
+    // Completes a single task
+    // Returns whether the operation has finished
+    protected boolean completeNextTask() {
+        BooleanSupplier task;
+        if((task = tasks.poll()) == null) {
+            callback.accept(true, null);
+            return true;
+        }
+
+        // If task failed, end operation
+        boolean failed = !task.getAsBoolean();
+        if(failed) {
+            callback.accept(false, FailReason.TASK_FAILED);
+        }
+        return failed;
+    }
+
+    public enum FailReason {
+        QUEUE_SHUTDOWN,
+        TASK_FAILED,
+        UNSPECIFIED
+    }
+}
