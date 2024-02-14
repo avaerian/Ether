4c884df9bcdb20cba7769bf344d078ce7f3bc465
avaerian <55154263+avaerian@users.noreply.github.com>
2024-02-13 22:16:41 -0500

    Implement Vec2i to Long adapter

--- main/src/main/java/org/minerift/ether/database/sql/adapters/Vec2i2LongAdapter.java
+++ main/src/main/java/org/minerift/ether/database/sql/adapters/Vec2i2LongAdapter.java
@@ -3,13 +3,14 @@ package org.minerift.ether.database.sql.adapters;
 import org.minerift.ether.math.Vec2i;
 
 public class Vec2i2LongAdapter implements Adapter<Vec2i, Long> {
+
     @Override
     public Long adaptTo(Vec2i obj) {
-        return null;
+        return ((long) obj.getX() << 32) | (obj.getZ() & 0xffffffffL);
     }
 
     @Override
     public Vec2i adaptFrom(Long obj) {
-        return null;
+        return new Vec2i((int)(obj >> 32), obj.intValue());
     }
 }
