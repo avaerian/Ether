8dcccc1d4292b86aae27dbfb546030aebf96e023
avaerian <55154263+avaerian@users.noreply.github.com>
2025-09-08 16:22:48 -0400

    Update paperweight paperDevBundle version (#17, #43)

--- nms/v1_20_R2/build.gradle.kts
+++ nms/v1_20_R2/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 dependencies {
     //paperweight.paperDevBundle("1.19.4-R0.1-20230608.201059-104")
-    paperweight.paperDevBundle("1.20.2-R0.1-20231119.012505-100")
+    paperweight.paperDevBundle("1.20.2-R0.1-SNAPSHOT")
 }
