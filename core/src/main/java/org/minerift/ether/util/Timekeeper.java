c08208a87c12824c718b013d344aa6e00707c8bd
Gabe <55154263+avaerian@users.noreply.github.com>
2025-10-30 17:37:51 -0400

    Create Timekeeper.java

---
+++ core/src/main/java/org/minerift/ether/util/Timekeeper.java
@@ -0,0 +1,10 @@
+package org.minerift.ether.util;
+
+public class Timekeeper {
+    // internals are public; use at own risk
+    public final int allowedSet;
+    public final int set;
+    public final long[] epochsNs;
+
+    
+}
