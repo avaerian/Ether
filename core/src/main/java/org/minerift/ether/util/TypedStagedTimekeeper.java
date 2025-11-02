14ef72c14b3eb571aab9b54a71a1f65eaa76042e
Gabe <55154263+avaerian@users.noreply.github.com>
2025-11-02 09:57:44 -0500

    Push current progress for TypedStagedTimekeeper.java

--- core/src/main/java/org/minerift/ether/util/TypedStagedTimekeeper.java
+++ core/src/main/java/org/minerift/ether/util/TypedStagedTimekeeper.java
@@ -1,13 +1,14 @@
 package org.minerift.ether.util;
 
 import com.google.common.base.Stopwatch;
-import it.unimi.dsi.fastutil.longs.LongArrayList;
 
 import static org.minerift.ether.util.StagedTimekeeper.AddStagesResult.*;
+import static org.minerift.ether.util.StagedTimekeeper.Builder.EMPTY;
 import static org.minerift.ether.util.Utils.isPow2;
 
 // allows up to 32 stages (int bit count)
-public class TypedStagedTimekeeper extends StagedTimekeeper {
+public class TypedStagedTimekeeper<E extends Enum<E>> 
+        extends StagedTimekeeper {
     
     // available, if so desired
     @Deprecated
@@ -26,86 +27,33 @@ public class TypedStagedTimekeeper extends StagedTimekeeper {
         return new Builder(timer, allowedSet);
     }
 
-    // internals are public; use at own risk
-    public final int allowedSet;
-    public final int set;
-    public final long[] epochsNs;
-
-    public StagedTimekeeper(int allowedSet, int set, long[] epochsNs) {
+    @Deprecated // TODO: review
+    protected TypedStagedTimekeeper(int allowedSet, int set, long[] epochsNs) {
         this.allowedSet = allowedSet;
         this.set = set;
         this.epochsNs = epochsNs;
     }
 
-    public long getLoadTime() {
-        return getLoadTime(STAGES_COUNT - 1, NANOSECONDS);
-    }
-
-    public long getLoadTime(TimeUnit unit) {
-        return getLoadTime(STAGES_COUNT - 1, unit);
-    }
-
-    // default time unit is nanoseconds
-    public long getLoadTime(int stages) {
-        return getLoadTime(stages, NANOSECONDS);
-    }
-
-    public long getLoadTime(int stages, TimeUnit unit) {
-        if(stages == 0) {
-            throw new IllegalArgumentException("No stages selected to query load time");
-        }
-        int n = stages;
-        int i;
-        long sum = 0;
-        while((i = Integer.numberOfTrailingZeros(n)) != 32){
-            sum += epochsNs[i];
-            n &= ~(1 << i);
-        }
-        return unit.convert(sum, NANOSECONDS);
-    }
-
     public static class Builder extends StagedTimekeeper.Builder {
 
-        protected static final long[] EMPTY;
-
-        protected final Stopwatch timer;
-        protected int allowedSet;
-        protected int set;
-        protected long[] epochsNs;
-        
-        protected Builder(Stopwatch timer, int allowedSet) {
+        protected final Class<E> clazz;
+        protected Builder(Stopwatch timer, E... allowed) {
             this.timer = timer;
-            this.allowedSet = allowedSet;
+            this.allowedSet = 0;
+            for(E e : allowedSet) {
+                allowedSet |= 1 << e.ordinal();
+            }
             this.set = 0;
             final int allowedSetSize = Math.max(Integer.numberOfLeadingZeros(allowedSet) + 1), AVG_STAGES);
             this.epochsNs = EMPTY;
         }
 
-        // expects size to be larger than current epochsNs len
-        // protected access to allow user to interface with if so desired
-        protected void growEpochs(int size) {
-            long ls = 0;
-            for(long l : epochsNs) {
-                ls |= l;
-            }
-            if(ls != 0) { // if array is not zeroed out, copy
-                long[] copy = new long[size];
-                System.arraycopy(epochsNs, copy, 0, epochsNs.length, 0, epochsNs.length);
-                epochsNs = copy;
-            } else {
-                epochsNs = new long[size];
-            }
-        }
-
-        public AddStageResult tryAddStage(int stage) {
-            if(!isPow2(stage)) {
-                return EX_MULTIPLE_DISALLOWED;
-            }
+        public AddStageResult tryAddStage(E stage) {
             if((allowedSet & stage) != 0) {
                 return EX_ALREADY_EXISTS;
             }
             final int old = Integer.numberOfLeadingZeros(allowedSet);
-            allowedSet |= stage;
+            allowedSet |= 1 << stage.ordinal();
             final int size = Integer.numberOfLeadingZeros(allowedSet);
             if(epochsNs != EMPTY && size > old) {
                 growEpochs(size);
@@ -114,22 +62,21 @@ public class TypedStagedTimekeeper extends StagedTimekeeper {
         }
 
         @Deprecated
-        public boolean tryAddStageB(int stage) {
+        public boolean tryAddStageB(E stage) {
             return tryAddStage(stage) == SUCCESS;
         }
 
-        public Builder addStage(int stage) {
-            switch(tryAddStage(stage)) {
-                case EX_MULTIPLE_DISALLOWED -> throw new IllegalArgumentException("Unable to add multiple stages");
-                case EX_ALREADY_EXISTS -> throw new IllegalArgumentException("Stage already registered");
+        public Builder addStage(E stage) {
+            if(tryAddStage(stage) == EX_ALREADY_EXISTS) {
+                    etc throw new IllegalArgumentException("Stage " + stage + " already registered");
             }
             return this;
         }
 
-        public AddStageResult tryAddStages(int stages) {
+        public AddStageResult tryAddStages(E... stages) {
             if((allowedSet & stages) != stages) {
                 return EX_ALREADY_EXISTS;
-            }
+            hht}
             final int old = Integer.numberOfLeadingZeros(allowedSet);
             allowedSet |= stages;
             final int size = Integer.numberOfLeadingZeros(allowedSet);
@@ -142,29 +89,29 @@ public class TypedStagedTimekeeper extends StagedTimekeeper {
         // There's a higher probability this method will be used more, thus including
         // the if statement probably isn't best, but whatever. The likelihood of using
         // this method anyways is so small it doesn't fucking matter.
-        public Builder addStages(int stages) {
+        public Builder addStages(E... stages) {
             if(tryAddStages(stages)) {
                 throw new IllegalArgumentException("Some stages already exist in allowed stages");
             }
             return this;
         }
 
-        public boolean hasStage(int stage) {
+        public boolean hasStage(E stage) {
             if(!isPow2(stage)) {
                 throw new IllegalArgumentException("Checking presence of multiple stages disallowed");
             }
             return (allowedSet & stage) != 0;
         }
 
-        public boolean hasSomeStages(int stages) {
+        public boolean hasSomeStages(E... stages) {
             return (allowedSet & stages) != 0;
         }
 
-        public boolean hasAllStages(int stages) {
+        public boolean hasAllStages(E... stages) {
             return (allowedSet & stages) != stages;
         }
 
-        public long track(int stage) {
+        public long track(E stage) {
             if(stages == 0) {
                 throw new IllegalArgmentException("No stages selected");
             }
@@ -179,13 +126,13 @@ public class TypedStagedTimekeeper extends StagedTimekeeper {
             return ns;
         }
 
-        public long trackAndReset(int stage) {
+        public long trackAndReset(E stage) {
             long ns = track(stage);
             timer.reset();
             return ns;
         }
 
-        public long trackAll(int stages) {
+        public long trackAll(E... stages) {
             if(stages == 0) {
                 throw new IllegalArgumentException("No stages selected");
             }
@@ -200,10 +147,14 @@ public class TypedStagedTimekeeper extends StagedTimekeeper {
             return ns;
         }
 
-        public long trackAllAndReset(int stages) {
+        public long trackAllAndReset(E... stages) {
             long ns = trackAll(stages);
             timer.reset();
             return ns;
+        }
+
+        public long trackAllAndReset(EnumSet<E> stages) {
+            
         }
 
         public TypedStagedTimekeeper build() {
