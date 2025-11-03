75ed045ef61e87a81d4b738296a9a5418811ba5a
Gabe <55154263+avaerian@users.noreply.github.com>
2025-11-03 11:00:47 -0500

    Update EtherLoadException.java

--- core/src/main/java/org/minerift/ether/EtherLoadException.java
+++ core/src/main/java/org/minerift/ether/EtherLoadException.java
@@ -1,9 +1,6 @@
 package org.minerift.ether;
 
 public class EtherLoadException extends Exception {
-    package org.minerift.ether;
-
-public class EtherLoadException extends Exception {
     public EtherLoadException() {
         super();
     }
