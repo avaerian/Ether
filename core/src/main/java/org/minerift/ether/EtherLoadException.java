8cfbf3b67c27283dd3718c0e1b1381d97331c478
Gabe <55154263+avaerian@users.noreply.github.com>
2025-11-03 11:00:17 -0500

    Create EtherLoadException.java

---
+++ core/src/main/java/org/minerift/ether/EtherLoadException.java
@@ -0,0 +1,22 @@
+package org.minerift.ether;
+
+public class EtherLoadException extends Exception {
+    package org.minerift.ether;
+
+public class EtherLoadException extends Exception {
+    public EtherLoadException() {
+        super();
+    }
+
+    public EtherLoadException(String message) {
+        super(message);
+    }
+
+    public EtherLoadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EtherLoadException(Throwable cause) {
+        super(cause);
+    }
+}
