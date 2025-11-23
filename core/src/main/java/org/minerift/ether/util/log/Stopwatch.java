02619762ff16f1d5bdd70b41d95290a2201cd6c0
Gabe <55154263+avaerian@users.noreply.github.com>
2025-11-22 20:39:36 -0500

    Minor syntax fixes

--- core/src/main/java/org/minerift/ether/util/log/Stopwatch.java
+++ core/src/main/java/org/minerift/ether/util/log/Stopwatch.java
@@ -8,7 +8,7 @@ public class Stopwatch {
         return new Stopwatch();
     }
 
-    // this timer solution only works for approximately the next 263 years
+    // this timer solution only works for approximately the next 108 years
     // for nanoseconds as our time unit, so if we want to extend the
     // allowed amount of time we could probably just add a separate 
     // flag variable or something, but this is fine for now (or, even 
@@ -26,7 +26,7 @@ public class Stopwatch {
     public void start() {
         long currNs = System.nanoTime();
         if(startNs < 0 && startNs != UNSTARTED) {
-            startNs = currNs - (~(1 <<< 63) & startNs);
+            startNs = currNs - (~(1 << 63) & startNs);
         } else {
             startNs = currNs;
         }
@@ -49,7 +49,7 @@ public class Stopwatch {
     // 
     // return elapsed time?
     public void stop() {
-        startNs = (System.nanoTime() - startNs) | (1 <<< 63);
+        startNs = (System.nanoTime() - startNs) | (1 << 63);
     }
     
     // return elapsed time?
@@ -72,7 +72,7 @@ public class Stopwatch {
         } else if (started < 0) { // paused flag is set
             // there's a constant, but too many fucking FF's so not gonna bother writing out for 64 bits
             // paused timer now equals elapsed time, so disregard paused flag for elapsed time
-            return ~(1 <<< 63) & startNs;
+            return ~(1 << 63) & startNs;
         } else { // actively running
             return System.nanoTime() - startNs;
         }
