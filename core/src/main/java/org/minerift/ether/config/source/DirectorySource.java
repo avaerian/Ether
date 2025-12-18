a3a3efc03c170bfe0474fa4917d8b006ef951be7
avaerian <55154263+avaerian@users.noreply.github.com>
2025-12-17 20:22:32 -0500

    Implement early test Source types to explore concept (#69, #32)

--- core/src/main/java/org/minerift/ether/config/source/DirectorySource.java
+++ core/src/main/java/org/minerift/ether/config/source/DirectorySource.java
@@ -1,5 +1,33 @@
 package org.minerift.ether.config.source;
 
-public class DirectorySource {
+import java.io.File;
 
+public class DirectorySource implements Source {
+
+    public static DirectorySource of(File dir) {
+        if(!dir.isDirectory()) {
+            throw new IllegalArgumentException("File must be a directory");
+        }
+        return new DirectorySource(dir);
+    }
+
+    private File dir;
+
+    public DirectorySource(File dir) {
+        this.dir = dir;
+    }
+
+    public File getDirectory() {
+        return dir;
+    }
+
+    @Override
+    public boolean create() {
+        return false;
+    }
+
+    @Override
+    public boolean exists() {
+        return false;
+    }
 }
