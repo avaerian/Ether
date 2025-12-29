83cfe0d928e8146e42518eb9a6cc7ae6f2a94ebd
avaerian <55154263+avaerian@users.noreply.github.com>
2025-12-29 00:45:34 -0500

    Begin reviewing BiomeVolume (#22, #27)

--- core/src/main/java/org/minerift/ether/schematic/data/BiomeVolume.java
+++ core/src/main/java/org/minerift/ether/schematic/data/BiomeVolume.java
@@ -11,10 +11,6 @@ public class BiomeVolume extends Volume<Biome<?>> {
         return new BiomeVolume.Builder();
     }
 
-    public static BiomeVolume.Lazy ofLazy() {
-        return new BiomeVolume.Lazy(null, null, null, 0,0,0);
-    }
-
     public BiomeVolume(Array3DOrder order, byte[] data, BytePalette<Biome<?>> palette, int width, int height, int length) {
         super(order, data, palette, width, height, length);
     }
@@ -35,42 +31,6 @@ public class BiomeVolume extends Volume<Biome<?>> {
         @Override
         public BiomeVolume build() {
             return new BiomeVolume(order, data, palette, width, height, length);
-        }
-    }
-
-    @NotWorking
-    @Deprecated // needs a lot of work
-    public static class Lazy extends BiomeVolume {
-
-        public static final byte[] EMPTY_DATA = new byte[0];
-
-        @Override
-        public boolean isEmpty() {
-            return super.isEmpty();
-        }
-
-        @Override
-        public ByteIterator byteIterator() {
-            return super.byteIterator();
-        }
-
-        @Override
-        public byte setData(byte b, int x, int y, int z) {
-            return super.setData(b, x, y, z);
-        }
-
-        @Override
-        public byte[] getData() {
-            return super.getData();
-        }
-
-        @Override
-        public Biome<?> getDataAt(int idx) {
-            return super.getDataAt(idx);
-        }
-
-        public Lazy(Array3DOrder order, byte[] data, BytePalette<Biome<?>> palette, int width, int height, int length) {
-            super(order, EMPTY_DATA, palette, width, height, length);
         }
     }
 }
