e651b8047fa58d9cfca262d067d873670097b821
avaerian <55154263+avaerian@users.noreply.github.com>
2025-12-31 01:10:29 -0500

    Implement a synchronized IslandGrid wrapper (#58, #74)

--- core/src/main/java/org/minerift/ether/island/IslandGrid.java
+++ core/src/main/java/org/minerift/ether/island/IslandGrid.java
@@ -14,6 +14,10 @@ import java.util.concurrent.locks.Lock;
 //  more on this soon
 public interface IslandGrid {
 
+    static Synchronized synchronize(IslandGrid grid) {
+        return new Synchronized(grid);
+    }
+
     // Registers an island onto the grid.
     // If the island id exists in the grid already and the island is deleted, the island will be replaced.
     void registerIsland(Island island) throws IllegalStateException;
@@ -49,5 +53,88 @@ public interface IslandGrid {
     Lock readLock();
     Lock writeLock();
 
+    class Synchronized implements IslandGrid {
+
+        private IslandGrid grid; // backing grid
+
+        private Synchronized(IslandGrid grid) {
+            this.grid = grid;
+        }
+
+        @Override
+        public synchronized void registerIsland(Island island) throws IllegalStateException {
+            grid.registerIsland(island);
+        }
+
+        @Override
+        public synchronized void unregisterIsland(int id) {
+            grid.unregisterIsland(id);
+        }
+
+        @Override
+        public synchronized void unregisterIsland(Island island) {
+            grid.unregisterIsland(island);
+        }
+
+        @Override
+        public synchronized Optional<Island> getIslandAt(int id) {
+            return grid.getIslandAt(id);
+        }
+
+        @Override
+        public synchronized Optional<Island> getIslandAt(Vec2i tile) {
+            return grid.getIslandAt(tile);
+        }
+
+        @Override
+        public synchronized boolean isTileOccupied(int id) {
+            return grid.isTileOccupied(id);
+        }
+
+        @Override
+        public synchronized boolean isTileOccupied(Vec2i tile) {
+            return grid.isTileOccupied(tile);
+        }
+
+        @Override
+        public synchronized boolean needsClearing(Vec2i tile) {
+            return grid.needsClearing(tile);
+        }
+
+        @Override
+        public synchronized int getIslandCount() {
+            return grid.getIslandCount();
+        }
+
+        @Override
+        public synchronized ImmutableList<Island> getIslandsView() {
+            return grid.getIslandsView();
+        }
+
+        @Override
+        public synchronized ImmutableList<Vec2i> getAvailableTiles() {
+            return grid.getAvailableTiles();
+        }
+
+        @Override
+        public synchronized Vec2i getNextTile() {
+            return grid.getNextTile();
+        }
+
+        @Override
+        public synchronized IntSet getIslandIds() {
+            return grid.getIslandIds();
+        }
+
+        @Override
+        public synchronized Lock readLock() {
+            return grid.readLock();
+        }
+
+        @Override
+        public synchronized Lock writeLock() {
+            return grid.writeLock();
+        }
+    }
 
 }
