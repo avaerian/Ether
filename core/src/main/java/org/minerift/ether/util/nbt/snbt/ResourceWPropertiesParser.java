a27bec888b12462d4f2825048b90a5ac08f7a4b0
avaerian <55154263+avaerian@users.noreply.github.com>
2025-12-31 21:17:10 -0500

    Init data component format parser class (#76)

---
+++ core/src/main/java/org/minerift/ether/util/nbt/snbt/ResourceWPropertiesParser.java
@@ -0,0 +1,15 @@
+package org.minerift.ether.util.nbt.snbt;
+
+import org.minerift.ether.debug.Debug;
+
+// reference: https://minecraft.wiki/w/Data_component_format#Command_format
+public class ResourceWPropertiesParser {
+
+    @Debug
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
