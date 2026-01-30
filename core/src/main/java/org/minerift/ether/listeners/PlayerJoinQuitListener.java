622c21e149c940d51ff35eb464bd7eae0cccc916
avaerian <55154263+avaerian@users.noreply.github.com>
2026-01-30 11:25:45 -0500

    Begin implementing appropriate user management w/ db support

--- core/src/main/java/org/minerift/ether/listeners/PlayerJoinQuitListener.java
+++ core/src/main/java/org/minerift/ether/listeners/PlayerJoinQuitListener.java
@@ -5,34 +5,54 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.minerift.ether.Ether;
+import org.minerift.ether.database.Result;
+import org.minerift.ether.database.models.UserModel;
 import org.minerift.ether.island.IslandRole;
 import org.minerift.ether.user.EtherUser;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.minerift.ether.island.Island.INVALID_ID;
 
 public class PlayerJoinQuitListener implements Listener {
 
     @EventHandler
     public void onPlayerLogin(AsyncPlayerPreLoginEvent event) {
         UUID uuid = event.getUniqueId();
-        // TODO: attempt to fetch data from database, otherwise create new profile
 
-        // Check if user is already registered
-        if(Ether.inst().getUserManager().getUser(uuid).isPresent()) {
-            return;
-        }
+        AtomicReference<EtherUser> user = new AtomicReference<>();
+        Ether.inst().getDatabase().accessSync((access) -> {
+            Result<EtherUser> res = access.selectById(UserModel.class, uuid);
+            if(res.isEmpty()) {
+                user.set(EtherUser.builder()
+                        .setUUID(uuid)
+                        .setIsland(INVALID_ID)
+                        .setIslandRole(IslandRole.VISITOR)
+                        .build());
+                access.insert(UserModel.class, user.get());
+            } else {
+                user.set(res.getRecord(0).read());
+            }
+        }).join();
 
-        EtherUser user = EtherUser.builder()
-                .setUUID(uuid)
-                .setIsland(null)
-                .setIslandRole(IslandRole.VISITOR)
-                .build();
-
-        Ether.inst().getUserManager().register(user);
+        Ether.inst().getUserManager().register(user.get());
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
+
+        EtherUser user = Ether.inst().getUserManager().getUser(event.getPlayer().getUniqueId()).orElseThrow();
+        Ether.inst().getDatabase().access((access) -> {
+            Result<EtherUser> oldUser = access.selectById(UserModel.class, user.getUUID());
+            if(oldUser.isEmpty()) {
+                access.insert(UserModel.class, user);
+            } else {
+                access.update(UserModel.class, user);
+            }
+
+            Ether.inst().getUserManager().unregister(user);
+        });
 
     }
 
--- core/src/main/java/org/minerift/ether/user/UserManager.java
+++ core/src/main/java/org/minerift/ether/user/UserManager.java
@@ -1,19 +1,23 @@
 package org.minerift.ether.user;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
-// TODO: needs work
 public class UserManager {
 
     private Map<UUID, EtherUser> users;
 
     public UserManager() {
-        this.users = new HashMap<>();
+        this.users = new ConcurrentHashMap<>();
     }
 
     // Returns whether the user was registered successfully
     public boolean register(EtherUser user) {
         return users.putIfAbsent(user.getUUID(), user) == null;
+    }
+
+    public void unregister(EtherUser user) {
+        users.remove(user.getUUID());
     }
 
     public Optional<EtherUser> getUser(UUID uuid) {
